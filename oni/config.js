307c54b4ef57e82843480366d772847a3b22e642
Nick Willett-Jeffries <nwillettjeffries@gmail.com>
2020-04-25 14:36:03 -0400

    Remove config for things I no longer use

--- oni/config.js
+++
@@ -1,7 +0,0 @@
-module.exports = {
-    "autoUpdate.enabled": true,
-    "achievements.enabled": false,
-    "browser.enabled": false,
-    "learning.enabled": false,
-    "sidebar.default.open": false
-};
