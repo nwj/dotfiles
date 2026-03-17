2e53f371df67ab3346dccf9ca72bf24fc5c29c2c
Nick Willett-Jeffries <nwillettjeffries@gmail.com>
2026-03-16 20:18:35 -0400

    Add Grug Far to neovim

--- personal/nvim/init.lua
+++ personal/nvim/init.lua
@@ -124,6 +124,21 @@ require("lazy").setup({
 		},
 	},
 
+	-- Grug Far (Global Find-and-Replace) setup
+	{
+		"MagicDuck/grug-far.nvim",
+		-- Note (lazy loading): grug-far.lua defers all it's requires so it's lazy by default
+		-- additional lazy config to defer loading is not really needed...
+		config = function()
+			-- optional setup call to override plugin options
+			-- alternatively you can set options with vim.g.grug_far = { ... }
+			require("grug-far").setup({
+				-- options, see Configuration section below
+				-- there are no required options atm
+			})
+		end,
+	},
+
 	-- Gitsigns setup
 	{
 		"lewis6991/gitsigns.nvim",
