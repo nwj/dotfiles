c4ccede3eadfc47dc39eeb2e38670fe4dbe70d68
Nick Willett-Jeffries <nwillettjeffries@gmail.com>
2026-03-16 20:42:30 -0400

    Remove setup for alacritty, helix, tmux

--- personal/setup.sh
+++ personal/setup.sh
@@ -3,9 +3,6 @@
 WORKING_DIRECTORY=$(pwd)
 XDG_CONFIG_HOME=$HOME/.config
 
-mkdir -p "$XDG_CONFIG_HOME/alacritty"
-ln -sf "$WORKING_DIRECTORY/alacritty/alacritty.toml" "$XDG_CONFIG_HOME/alacritty/alacritty.toml"
-
 mkdir -p "$XDG_CONFIG_HOME/ghostty"
 ln -sf "$WORKING_DIRECTORY/ghostty/config" "$XDG_CONFIG_HOME/ghostty/config"
 
@@ -15,13 +12,6 @@ ln -sf "$WORKING_DIRECTORY/git/config" "$XDG_CONFIG_HOME/git/config"
 mkdir -p "$XDG_CONFIG_HOME/nvim"
 ln -sf "$WORKING_DIRECTORY/nvim/init.lua" "$XDG_CONFIG_HOME/nvim/init.lua"
 ln -sfn "$WORKING_DIRECTORY/nvim/autoload" "$XDG_CONFIG_HOME/nvim/autoload"
-
-mkdir -p "$XDG_CONFIG_HOME/helix"
-ln -sf "$WORKING_DIRECTORY/helix/config.toml" "$XDG_CONFIG_HOME/helix/config.toml"
-ln -sf "$WORKING_DIRECTORY/helix/languages.toml" "$XDG_CONFIG_HOME/helix/languages.toml"
-
-mkdir -p "$XDG_CONFIG_HOME/tmux"
-ln -sf "$WORKING_DIRECTORY/tmux/tmux.conf" "$XDG_CONFIG_HOME/tmux/tmux.conf"
 
 mkdir -p "$XDG_CONFIG_HOME/fish"
 ln -sf "$WORKING_DIRECTORY/fish/config.fish" "$XDG_CONFIG_HOME/fish/config.fish"
